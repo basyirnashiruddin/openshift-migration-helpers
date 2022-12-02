14043cb11400ec437bd053e61ea3a9c0feaf9556
Basyir Nashiruddin Al Bantani <basyirnashiruddin@gmail.com>
2022-12-02 15:24:05 +0700

    Initiate release

---
+++ getdeploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'PARAM=($(cat object.conf))'
+NAMESPACE=$1
+echo "EXPORTING PROJECT: $NAMESPACE"
+mkdir -p export
+for OBJ_TYPE in ${PARAM[@]}
+do
+	echo "PROCESS EXPORTING $OBJ_TYPE"
+	EXECUTE=`oc get $OBJ_TYPE -n $NAMESPACE -o jsonpath="{.items[*]['metadata.name']}"`
+	OBJECTS=$(echo $EXECUTE | tr " " "\n")
+	for OBJ in $OBJECTS
+	do
+    	oc get $OBJ_TYPE/$OBJ -o yaml | yq eval 'del(.metadata.resourceVersion, .metadata.uid, .metadata.annotations, .metadata.creationTimestamp, .metadata.selfLink, .metadata.managedFields, .status, .metadata.ownerReferences, .image.metadata.resourceVersion, .image.metadata.uid, .image.metadata.annotations, .image.metadata.creationTimestamp)' - > export/$OBJ_TYPE.$OBJ.yml
+	done
+done
