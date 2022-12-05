fe518e40125f4f4e27e9fcc75cc949b55c5accfc
basyirnashiruddin@gmail.com <basyirnashiruddin@gmail.com>
2022-12-05 17:20:43 +0700

    Update token

--- migrateimage.sh
+++ migrateimage.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 file="./access.conf"
 while IFS='=' read -r key value
 do
@@ -17,13 +17,12 @@ podman login -u admin -p $DST_TOKEN $DST_REG --tls-verify=false
 
 echo "Starting Image Migration"
 
-ISTAGS=`oc get is -o jsonpath="{range .items[*]}{@.metadata.name}{':'}{@.status.tags[0].tag}{'\n'}{end}" -n $NAMESPACE`
+ISTAGS=`oc get is -o jsonpath="{range .items[*]}{@.metadata.name}{':'}{@.status.tags[0].tag}{'\n'}{end}" -n $NAMESPACE --token=$SRC_TOKEN`
 for ISTAG in $ISTAGS
 do
         IZ=${ISTAG: -1}
         if [ "$IZ" != ":" ]; then
                 echo "Migrating: $ISTAG"
-                echo "oc image mirror -a $XDG_RUNTIME_DIR/containers/auth.json $SRC_REG/$NAMESPACE/$ISTAG $DST_REG/$NAMESPACE/$ISTAG --insecure=true"
                 oc image mirror -a $XDG_RUNTIME_DIR/containers/auth.json $SRC_REG/$NAMESPACE/$ISTAG $DST_REG/$NAMESPACE/$ISTAG --insecure=true
         fi
 done
