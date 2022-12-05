af947fd3613e792b0a946ee3ca2df1690a15ad00
basyirnashiruddin@gmail.com <basyirnashiruddin@gmail.com>
2022-12-05 16:47:18 +0700

    Add image migrator

--- getdeploy.sh
+++ getdeploy.sh
@@ -8,8 +8,9 @@ do
 	echo "PROCESS EXPORTING $OBJ_TYPE"
 	EXECUTE=`oc get $OBJ_TYPE -n $NAMESPACE -o jsonpath="{.items[*]['metadata.name']}"`
 	OBJECTS=$(echo $EXECUTE | tr " " "\n")
+	mkdir -p export/$OBJ_TYPE
 	for OBJ in $OBJECTS
 	do
-    	oc get $OBJ_TYPE/$OBJ -o yaml | yq eval 'del(.metadata.resourceVersion, .metadata.uid, .metadata.annotations, .metadata.creationTimestamp, .metadata.selfLink, .metadata.managedFields, .status, .metadata.ownerReferences, .image.metadata.resourceVersion, .image.metadata.uid, .image.metadata.annotations, .image.metadata.creationTimestamp)' - > export/$OBJ_TYPE.$OBJ.yml
+    	oc get $OBJ_TYPE/$OBJ -o yaml | yq eval 'del(.metadata.resourceVersion, .metadata.uid, .metadata.annotations, .metadata.creationTimestamp, .metadata.selfLink, .metadata.managedFields, .status, .metadata.ownerReferences, .image.metadata.resourceVersion, .image.metadata.uid, .image.metadata.annotations, .image.metadata.creationTimestamp)' - > export/$OBJ_TYPE/$OBJ.yml
 	done
 done
---
+++ migrateimage.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+file="./access.conf"
+while IFS='=' read -r key value
+do
+    key=$(echo $key | tr '.' '_')
+    eval ${key}=\${value}
+done < "$file"
+
+NAMESPACE=$1
+SRC_REG=${source_registry}
+SRC_TOKEN=${source_token}
+DST_REG=${destination_registry}
+DST_TOKEN=${destination_token}
+
+podman login -u admin -p $SRC_TOKEN $SRC_REG --tls-verify=false
+podman login -u admin -p $DST_TOKEN $DST_REG --tls-verify=false
+
+echo "Starting Image Migration"
+ISTAGS=`oc get is -o jsonpath="{range .items[*]}{@.metadata.name}{':'}{@.status.tags[0].tag}{'\n'}{end}" -n $NAMESPACE`
+for ISTAG in $ISTAGS
+do
+        echo "Migrating: $ISTAG"
+        IZ=${ISTAG: -1}
+        if [ "$IZ" != ":" ]; then
+                oc image mirror -a $XDG_RUNTIME_DIR/containers/auth.json $SRC_REG/$NAMESPACE/$ISTAG $DST_REG/$NAMESPACE/$ISTAG --insecure=true
+        fi
+done
