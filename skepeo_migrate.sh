fde0b15fe1be85ecdce3b3a5be370646995e49fc
basyirnashiruddin@gmail.com <basyirnashiruddin@gmail.com>
2022-12-06 15:12:32 +0700

    Using token

--- skepeo_migrate.sh
+++ skepeo_migrate.sh
@@ -14,14 +14,11 @@ DST_REG=${destination_registry}
 DST_TOKEN=${destination_token}
 DST_USER=${destination_user}
 
-skopeo login --username $SRC_USER --password $SRC_TOKEN $SRC_REG --tls-verify=false
-skopeo login --username $DST_USER --password $DST_TOKEN $DST_REG --tls-verify=false
-
 echo "Starting Image Migration"
 
-ISTAGS=`oc get istag -o jsonpath="{range .items[*]}{@.metadata.name}{'\n'}{end}" -n $NAMESPACE --token=$SRC_TOKEN`
+ISTAGS=`oc get is -o jsonpath="{range .items[*]}{@.metadata.name}{'\n'}{end}" -n $NAMESPACE --token=$SRC_TOKEN`
 for ISTAG in $ISTAGS
 do
         echo "Migrating: $ISTAG"
-        skopeo sync --src docker $SRC_REG/$NAMESPACE/$ISTAG --src-tls-verify=false --dest docker $DST_REG/$NAMESPACE/$ISTAG --dest-tls-verify=false
+        skopeo sync --src docker $SRC_REG/$NAMESPACE/$ISTAG --src-tls-verify=false --dest docker $DST_REG/$NAMESPACE/$ISTAG --dest-tls-verify=false --src-tls-verify=false --src-registry-token $SRC_TOKEN --dest-registry-token $DST_TOKEN
 done
